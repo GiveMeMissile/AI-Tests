f45fc7ed3b563d6f023d7708dec26adc0b02c2c4
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-12-09 20:21:29 -0600

    Update Test_1.py
    Small improvement update

--- Test_1.py
+++ Test_1.py
@@ -4,9 +4,8 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-NUMBER_OF_OUTPUTS = 1
-NUMBER_OF_CHOICES = 2
-BATCH_SIZE = 40
+NUMBER_OF_CHOICES = 1
+BATCH_SIZE = 16
 IMAGE_DIMENSIONS = 64
 NEURONS = 2674
 DEVICE = "cpu"  # Use "cuda" if you have CUDA and want to use the GPU
@@ -17,22 +16,17 @@ transform_bad = transforms.Compose([
     transforms.Normalize(mean=[0.3598, 0.4181, 0.4291], std=[0.2876, 0.2580, 0.2811]),
     transforms.RandomHorizontalFlip(),
     transforms.RandomVerticalFlip(),
-    transforms.RandomRotation((-180, 180)),
-    transforms.Grayscale()
+    transforms.RandomRotation((-180, 180))
 ])
 
 transform_test = transforms.Compose([
     transforms.Resize((IMAGE_DIMENSIONS, IMAGE_DIMENSIONS)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.4282, 0.4245, 0.3755], std=[0.3002, 0.2806, 0.2890]),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomVerticalFlip(),
-    transforms.RandomRotation((-180, 180)),
-    transforms.Grayscale()
 ])
 
-bad_dataset = datasets.ImageFolder(root="Bad_data", transform=transform_bad)
-test_dataset = datasets.ImageFolder(root="Placeholder", transform=transform_test)
+bad_dataset = datasets.ImageFolder(root="Bad_data_edited", transform=transform_bad)
+test_dataset = datasets.ImageFolder(root="Bad_data", transform=transform_test)
 
 bad_dataloader = DataLoader(bad_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
@@ -43,7 +37,7 @@ class AI1(nn.Module):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(IMAGE_DIMENSIONS*IMAGE_DIMENSIONS, NEURONS),
+            nn.Linear(IMAGE_DIMENSIONS*IMAGE_DIMENSIONS*3, NEURONS),
             nn.ReLU(),
             nn.Linear(NEURONS, NEURONS),
             nn.ReLU(),
@@ -58,15 +52,8 @@ class AI1(nn.Module):
 
 model = AI1().to(DEVICE)
 
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
-X = torch.rand(1, IMAGE_DIMENSIONS, IMAGE_DIMENSIONS, device=DEVICE)
-logits = model(X)
-predict_probability = nn.Softmax(dim=1)(logits)
-y_predict = predict_probability.argmax(1)
-print(f"Predicted class: {y_predict}")
-
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -76,14 +63,14 @@ def train(dataloader, model, loss_fn, optimizer):
 
         # Forward pass
         prediction = model(X)
-        loss = loss_fn(prediction, y)
+        loss = loss_fn((prediction.squeeze(dim=1)).type(torch.float32), y.type(torch.float32))
 
         # Backward pass
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
 
-        if batch % 100 == 0:
+        if batch % 4 == 0:
             current = (batch + 1) * len(X)
             print(f"loss: {loss.item():>7f}  [{current:>5d}/{size:>5d}]")
 
@@ -93,12 +80,14 @@ def test(dataloader, model, loss_fn):
     num_batches = len(dataloader)
     model.eval()
     test_loss, correct = 0, 0
+    correct = 0
+    test_loss = 0
 
-    with torch.no_grad():
+    with torch.inference_mode():
         for X, y in dataloader:
             X, y = X.to(DEVICE), y.to(DEVICE)
             prediction = model(X)
-            test_loss += loss_fn(prediction, y).item()
+            test_loss += loss_fn(y.type(torch.float32), (prediction.squeeze(dim=1)).type(torch.float32)).item()
             correct += (prediction.argmax(1) == y).type(torch.float).sum().item()
 
     test_loss /= num_batches
@@ -106,7 +95,7 @@ def test(dataloader, model, loss_fn):
     print(f"Test Error: \n Accuracy: {100 * correct:>0.1f}%, Average Loss: {test_loss:>8f}")
 
 
-def getEpoch():
+def get_epoch():
     while True:
         try:
             epochs = int(input("How many epochs do you desire?: "))
@@ -119,17 +108,17 @@ def getEpoch():
     return epochs
 
 
-epochs = getEpoch()
-for t in range(epochs):
-    print(f"Epoch {t + 1}\n------------------------")
+epochs = get_epoch()
+for i in range(epochs):
+    print(f"Epoch {i + 1}\n------------------------")
     trainStart = time.time()
     train(bad_dataloader, model, loss_fn, optimizer)
     trainEnd = time.time()
-    print(f"Total train time: {trainEnd - trainStart}")
+    print(f"Total train time: {(trainEnd - trainStart):.4f}")
 
     testStart = time.time()
     test(test_dataloader, model, loss_fn)
     testEnd = time.time()
-    print(f"Total test time: {testEnd - testStart}")
+    print(f"Total test time: {(testEnd - testStart):.4f}")
 
 print("Finished!")
