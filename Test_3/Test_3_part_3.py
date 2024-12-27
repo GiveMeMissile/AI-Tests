f983aa9babe51c010a168df0ef37c064a2f0b5f6
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-12-27 09:34:01 -0600

    Update Test_3_part_3.py
    Added the capability to save models.

--- Test_3/Test_3_part_3.py
+++ Test_3/Test_3_part_3.py
@@ -2,6 +2,7 @@ import pygame
 import torch
 from torch import nn
 import random
+from pathlib import Path
 
 # window
 WINDOW_SIZE_X, WINDOW_SIZE_Y = 1100, 600
@@ -261,6 +262,22 @@ def simulation_display(goal, ai, target, ai_2):
     pygame.display.update()
 
 
+def save_model(model):
+    save = input("Do you want to save this model?: ")
+    if not (save == "yes" or save == "Yes"):
+        print("This model shall not be saved.")
+        return 0
+    model_path = Path(input("What file do you want to save your model in? \n"
+                            "If you input a file that dose not exist. One will be created for you: "))
+    model_path.mkdir(parents=True, exist_ok=True)
+    model_name = input("What do you want your model dict's name to be?: ")+".pth"
+    model_save_path = model_path/model_name
+    print("Now downloading the model.....")
+    # This will save the model dict. If you want to save the entire model then change this code to do so
+    torch.save(obj=model.state_dict(), f=model_save_path)
+    print("Model successfully saved! YIPPEE!!!")
+
+
 def main():
     running = True
     model_0 = AIModel(input_features=INPUT_FEATURES,
@@ -322,6 +339,10 @@ def main():
         simulation_display(goal, ai_object_0, target, ai_object_1)
     ai_object_0.print_results(current_time)
     ai_object_1.print_results(current_time)
+    print("Model 0 save")
+    save_model(model_0)
+    print("Model 1 save")
+    save_model(model_1)
 
 
 if __name__ == "__main__":
