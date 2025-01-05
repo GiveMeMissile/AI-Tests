65083e1e92d871175228bfb9bfe7fcd86f8dc2a9
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2025-01-04 22:55:48 -0600

    Update data_replication.py
    I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. I am so dumb. Why...

--- Test_5/data_replication.py
+++ Test_5/data_replication.py
@@ -2,24 +2,19 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from datasets import load_dataset, Image
-from transformers import AutoFeatureExtractor
 from torchvision import transforms
 from os import cpu_count
-from tqdm import tqdm
 
 NUM_WORKERS = cpu_count()
 TRAIN_DATA = "copycat-project/laion2b6plus_fish"
-PRETRAINED_EXTRACTOR = "google/vit-base-patch16-224-in21k"
 BATCH_SIZE = 16
 MAGNITUDE_BINS = 31
 TRANSFORM = transforms.Compose([
     transforms.Resize(size=(64, 64)),
-    transforms.TrivialAugmentWide(num_magnitude_bins=MAGNITUDE_BINS),
     transforms.ToTensor()
 ])
 
 
-# I wannacry
 def transform(examples):
     examples["pixel_values"] = [TRANSFORM(image) for image in examples["image"]]
     return examples
@@ -30,15 +25,10 @@ def get_data():
     dataset = dataset.with_format(type="torch")
     dataset = dataset.cast_column("image", Image(mode="RGB"))
     print(dataset)
-    dataset = dataset.set_transform(transform)
+    dataset.set_transform(transform)
     print(dataset)
     train_dataloader = DataLoader(dataset=dataset, batch_size=16, num_workers=NUM_WORKERS, shuffle=True)
-    replicated_train_dataloader = []
-    for batch in train_dataloader:
-        X = torch.randn(size=(16, 3, 64, 64))
-        replicated_train_dataloader.append(X)
-        print(batch)
-    return train_dataloader, replicated_train_dataloader
+    return train_dataloader
 
 
 if __name__ == "__main__":
