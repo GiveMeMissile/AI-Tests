4f658f0b575aaeaad7857f4627acad09e858ac37
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2025-01-06 14:46:20 -0600

    Update Data_Replication_Test_5.py
    Almost finished. Added the model and train and test functions. All I need to do is implement the train and test function in the training loop and add the save model function with a results displayer and the programming part shall be completed.

--- Test_5/Data_Replication_Test_5.py
+++ Test_5/Data_Replication_Test_5.py
@@ -1,15 +1,19 @@
 import torch
 from torch import nn
+from torch.utils.data import DataLoader
 from datasets import load_dataset, Image
 from torchvision import transforms
-from os import cpu_count
+from torchvision.datasets import ImageFolder
 from random import shuffle
+from time import time
 
-NUM_WORKERS = cpu_count()
+# CONSTANTS!!!
 TRAIN_DATA = "copycat-project/laion2b6plus_fish"
+TEST_DATA = "Bad_data"
 BATCH_SIZE = 32
-MAGNITUDE_BINS = 31
 IMAGE_DIMENSIONS = 64
+COLOR_CHANNELS = 3
+LEARNING_RATE = 0.001
 TRANSFORM = transforms.Compose([
     transforms.Resize(size=(IMAGE_DIMENSIONS, IMAGE_DIMENSIONS)),
     transforms.ToTensor()
@@ -18,9 +22,11 @@ TRANSFORM = transforms.Compose([
 
 class DataPreparer:
     def __init__(self, data):
+        # Stores a dataset to change
         self.dataset = data
 
     def shuffle_data(self, dataset=None):
+        # As the title suggests this function shuffles the data
         if dataset is None:
             dataset = self.dataset
         merged_data = list(zip(dataset["image"], dataset["category"]))
@@ -31,6 +37,7 @@ class DataPreparer:
         return dataset
 
     def batch_data(self, dataset=None):
+        # This function batches the data in order to train a neural network.
         if dataset is None:
             dataset = self.dataset
         dataset = self.tensorize_category(dataset)
@@ -55,6 +62,7 @@ class DataPreparer:
         return dataset
 
     def tensorize_category(self, dataset=None):
+        # This turns the category from strings to torch tensors that can be used in a Neural Network
         if dataset is None:
             dataset = self.dataset
         categories = []
@@ -70,10 +78,14 @@ class DataPreparer:
         return dataset
 
     def transform(self, examples):
+        # This transforms the huggingface dataset using the TRANSFORMER
         examples["pixel_values"] = [TRANSFORM(image) for image in examples["image"]]
         return examples
 
     def synthesize_data(self, dataset=None):
+        # This function creates images using torch.rand that mimic the non_fish category.
+        # This function returns a dictionary containing both the original images, random images,
+        # and the categories for both.
         if dataset is None:
             dataset = self.dataset
         print("\nSynthesizing data...")
@@ -81,7 +93,7 @@ class DataPreparer:
         replicated_category = []
 
         for i in range(len(dataset)):
-            X = torch.rand(size=(3, IMAGE_DIMENSIONS, IMAGE_DIMENSIONS))
+            X = torch.rand(size=(COLOR_CHANNELS, IMAGE_DIMENSIONS, IMAGE_DIMENSIONS))
             replicated_data.append(X)
             replicated_category.append("non_fish")
 
@@ -107,6 +119,7 @@ class DataPreparer:
         return data
 
     def prepare_data(self, dataset=None):
+        # This function takes a dataset and prepares it using the other functions in the class.
         if dataset is None:
             dataset = self.dataset
         dataset = self.synthesize_data(dataset)
@@ -115,7 +128,45 @@ class DataPreparer:
         return dataset
 
 
-def get_data():
+class AdaptiveCNNModel(nn.Module):
+    # I am using a model that I used in Test 2
+    # This cnn is made to be easily altered which makes it good for testing.
+    # And this model is capable of reaching a high accuracy as well.
+
+    def __init__(self, input_features, hidden_features, hidden_layers=1, output_features=1):
+        super().__init__()
+        self.input_layer = nn.Conv2d(in_channels=input_features, out_channels=hidden_features,
+                                     kernel_size=3, stride=1, padding=1)
+        self.hidden_layer = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_features, out_channels=hidden_features,
+                      kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_features, out_channels=hidden_features,
+                      kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_features, out_channels=hidden_features,
+                      kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.output_fixer = lambda length, layers: int((length*((1/2)**layers))**2)
+
+        self.output_layer = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_features*self.output_fixer(IMAGE_DIMENSIONS, hidden_layers),
+                      out_features=output_features)
+        )
+        self.hidden_layers = hidden_layers
+
+    def forward(self, x):
+        x = self.input_layer(x)
+        # This model can cycle through the hidden layer multiple times!!! (WOW)
+        for i in range(self.hidden_layers):
+            x = self.hidden_layer(x)
+        x = self.output_layer(x)
+        return x
+
+
+def get_huggingface_data():
     # This function gets the required dataset from hugging face and returns it.
     print("Getting huggingface dataset...")
     datasets = load_dataset(TRAIN_DATA, split="train")
@@ -126,10 +177,100 @@ def get_data():
     return datasets
 
 
+def get_test_data():
+    # This function gets the test data using the Bad_dataset.
+    # While the bad dataset is not the best dataset for testing due to its design.
+    # I lack anything better, yet it will be good enough as I am only looking for the direction of the model.
+    # Not the magnitude of its success.
+    dataset = ImageFolder(root=TEST_DATA, transform=TRANSFORM)
+    dataloader = DataLoader(dataset=dataset, batch_size=BATCH_SIZE, shuffle=False)
+    return dataloader
+
+
+def calculate_accuracy(y_logits, y):
+    y_pred = torch.sigmoid(y_logits)
+    y_pred = (y_pred > .5).int()
+    # print(y_pred == y)
+    accuracy = (y_pred == y).sum().item()
+    accuracy = accuracy/y.size(0)
+    return accuracy * 100
+
+
+def train(data, optimizer, loss_fn, model):
+    start = time()
+    model.train()
+
+    train_loss = 0
+    train_accuracy = 0
+
+    for batch, (X, y) in enumerate(zip(data["X"], data["y"])):
+        y_logits = model.forward(X)
+
+        # There is 100% gonna be a shape error lol
+        loss = loss_fn(y_logits, y)
+        train_loss += loss
+
+        optimizer.zero_grad()
+
+        loss.backward()
+        optimizer.step()
+
+        accuracy = calculate_accuracy(y_logits, y)
+        train_accuracy += accuracy
+
+    train_accuracy = train_accuracy/len(data["X"])
+    train_loss = train_loss/len(data["X"])
+
+    end = time()
+    total_time = end-start
+
+    return train_loss, train_accuracy, total_time
+
+
+def test(data, loss_fn, model):
+    start = time()
+    model.eval()
+
+    test_loss = 0
+    test_accuracy = 0
+
+    with torch.inference_mode():
+        for batch, (X, y) in enumerate(data):
+            y_logits = model.forward(X)
+
+            loss = loss_fn(y_logits, y)
+            test_loss += loss
+
+            accuracy = calculate_accuracy(y_logits, y)
+            test_accuracy += accuracy
+
+        test_loss = test_loss/len(data)
+        test_accuracy = test_accuracy/len(data)
+
+        end = time()
+        total_time = end-start
+
+        return test_loss, test_accuracy, total_time
+
+
 def main():
-    data = get_data()
+    data = get_huggingface_data()
     train_data_handler = DataPreparer(data=data)
-    train_dataset = train_data_handler.prepare_data()
+    train_data = train_data_handler.prepare_data()
+    test_data = get_test_data()
+    model = AdaptiveCNNModel(input_features=COLOR_CHANNELS, hidden_features=64)
+
+    loss_fn = nn.BCEWithLogitsLoss()  # Best loss function fr
+    optimizer = torch.optim.SGD(params=model.parameters(), lr=LEARNING_RATE)
+
+    epochs = 5
+
+    # Saving all the results in this dictionary for display later.
+    results = {"Epoch": [], "Train loss": [], "Train accuracy": [], "Train time": [], "Test loss": [],
+               "Test Accuracy": [], "Test time": []}
+
+    for epoch in range(epochs):
+        pass
 
 
 if __name__ == "__main__":
