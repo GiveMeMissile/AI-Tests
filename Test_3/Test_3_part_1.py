b7daf93ee61c52ab7308f283ca78355cb6088454
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2025-01-09 12:54:09 -0600

    Update Test_3_part_1.py
    Improved training. Now model training is much better yet not accurate to the other parts of the test. This will be changed later whenever I fixed all the other parts.

--- Test_3/Test_3_part_1.py
+++ Test_3/Test_3_part_1.py
@@ -5,49 +5,28 @@ import time
 from pathlib import Path
 
 
-WINDOW_SIZE_X, WINDOW_SIZE_Y = 800, 500
-NUM_TRAIN_TENSORS = 500
-NUM_TEST_TENSORS = 100
+WINDOW_SIZE_X, WINDOW_SIZE_Y = 1100, 600
+NUM_TRAIN_TENSORS = 5000
+NUM_TEST_TENSORS = 1000
 LENIENCY = 20
-INPUT_FEATURES = 2
-OUTPUT_FEATURES = 2
+INPUT_FEATURES = 4
+OUTPUT_FEATURES = 4
 USER_INPUT = True
+BATCH_SIZE = 32
 
 
-def create_data():
-    train_data = {"X": [], "y": []}
-    test_data = {"X": [], "y": []}
-    print("Creating training values...")
-    for _ in range(NUM_TRAIN_TENSORS):
+def create_data(num_tensors):
+    data = {"X": [], "y": []}
+
+    X_batch = []
+    y_batch = []
+
+    batch_mult = 1
+
+    for i in range(num_tensors):
         ai_x, ai_y = random.randint(0, WINDOW_SIZE_X), random.randint(0, WINDOW_SIZE_Y)
         goal_x, goal_y = random.randint(0, WINDOW_SIZE_X), random.randint(0, WINDOW_SIZE_Y)
-        X = torch.tensor([[ai_x, ai_y], [goal_x, goal_y]], dtype=torch.float32)
-        left = 0
-        right = 0
-        up = 0
-        down = 0
-        if goal_x - LENIENCY > ai_x:
-            right = 1
-        elif goal_x + LENIENCY < ai_x:
-            left = 1
-        else:
-            pass
-        if goal_y - LENIENCY > ai_y:
-            up = 1
-        elif goal_y + LENIENCY < ai_y:
-            down = 1
-        else:
-            pass
-        y = torch.tensor([up, down, left, right], dtype=torch.float32)
-        train_data["X"].append(X)
-        train_data["y"].append(y)
-    print("Training values created.")
-
-    print("Creating testing values...")
-    for _ in range(NUM_TEST_TENSORS):
-        ai_x, ai_y = random.randint(0, WINDOW_SIZE_X), random.randint(0, WINDOW_SIZE_Y)
-        goal_x, goal_y = random.randint(0, WINDOW_SIZE_X), random.randint(0, WINDOW_SIZE_Y)
-        X = torch.tensor([[ai_x, ai_y], [goal_x, goal_y]], dtype=torch.float32)
+        X = torch.tensor([[ai_x, ai_y, goal_x, goal_y]], dtype=torch.float32)
         left = 0
         right = 0
         up = 0
@@ -62,13 +41,19 @@ def create_data():
             down = 1
         elif goal_y < ai_y:
             up = 1
-        else:
-            pass
         y = torch.tensor([up, down, left, right], dtype=torch.float32)
-        test_data["X"].append(X)
-        test_data["y"].append(y)
-    print("Testing values created.")
-    return train_data, test_data
+        X_batch.append(X)
+        y_batch.append(y)
+        if i+1 == BATCH_SIZE*batch_mult or i+1 == NUM_TRAIN_TENSORS:
+            batch_mult += 1
+            X = torch.stack(X_batch, dim=0)
+            y = torch.stack(y_batch, dim=0)
+            data["X"].append(X)
+            data["y"].append(y)
+            X_batch.clear()
+            y_batch.clear()
+
+    return data
 
 
 class AIModel(nn.Module):
@@ -132,18 +117,12 @@ def get_user_input():
     return hidden_layers, hidden_features, learning_rate, epochs
 
 
-def calculate_acc(y_logits, y):
-    # This function calculates the accuracy of the AI model
-    y_pred = y_logits.sigmoid()
-    # This line of code below converts the tensors into booleans, compares the two,
-    # and then turns the tensor of booleans into a list that can be iterated through
-    acc_list = (y_pred.round() == y).tolist()
-    acc = 0
-    # This for loop below iterates through the list in order to calculate the accuracy
-    for equal in acc_list:
-        if equal:
-            acc += 25
-    return acc
+def calculate_accuracy(y_logits, y):
+    y_pred = torch.sigmoid(y_logits)
+    y_pred = (y_pred > .5).int()
+    accuracy = (y_pred == y).sum().item()
+    accuracy = accuracy/(y.size(0)*4)
+    return accuracy * 100
 
 
 def train(loss_fn, optimizer, model, train_dataset):
@@ -152,17 +131,15 @@ def train(loss_fn, optimizer, model, train_dataset):
     train_accuracy = 0
     for _, (X, y) in enumerate(zip(train_dataset["X"], train_dataset["y"])):
         y_logits = model.forward(X)
-        y_logits = torch.reshape(y_logits, (-1,))
-        loss = loss_fn(y_logits, y)
+        loss = loss_fn(y_logits.squeeze(dim=1), y)
         train_loss += loss
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        acc = calculate_acc(y_logits, y)
+        acc = calculate_accuracy(y_logits.squeeze(dim=1), y)
         train_accuracy += acc
-    print(train_accuracy)
-    train_loss = train_loss/NUM_TRAIN_TENSORS
-    train_accuracy = train_accuracy/NUM_TRAIN_TENSORS
+    train_loss = train_loss/len(train_dataset["X"])
+    train_accuracy = train_accuracy/len(train_dataset["X"])
     end = time.time()
     total_train_time = end-start
     return train_loss, train_accuracy, total_train_time
@@ -175,13 +152,12 @@ def test(loss_fn, model, test_dataset):
     with torch.inference_mode():
         for _, (X, y) in enumerate(zip(test_dataset["X"], test_dataset["y"])):
             y_logits = model.forward(X)
-            y_logits = torch.reshape(y_logits, (-1,))
-            loss = loss_fn(y_logits, y)
+            loss = loss_fn(y_logits.squeeze(dim=1), y)
             test_loss += loss
-            acc = calculate_acc(y_logits, y)
+            acc = calculate_accuracy(y_logits.squeeze(dim=1), y)
             test_accuracy += acc
-        test_loss = test_loss/NUM_TEST_TENSORS
-        test_accuracy = test_accuracy/NUM_TEST_TENSORS
+        test_loss = test_loss/len(test_dataset["X"])
+        test_accuracy = test_accuracy/len(test_dataset["X"])
         end = time.time()
         total_test_time = end-start
         return test_loss, test_accuracy, total_test_time
@@ -209,7 +185,8 @@ def save_model(model):
 
 # I am using a main function for once.
 def main():
-    train_dataset, test_dataset = create_data()
+    train_dataset = create_data(NUM_TRAIN_TENSORS)
+    test_dataset = create_data(NUM_TEST_TENSORS)
     hidden_layers, hidden_features, learning_rate, epochs = get_user_input()
     model = AIModel(input_features=INPUT_FEATURES, hidden_features=hidden_features,
                     hidden_layers=hidden_layers, output_features=OUTPUT_FEATURES)
@@ -219,9 +196,11 @@ def main():
     for epoch in range(epochs):
         train_loss, train_accuracy, total_train_time = train(loss_fn, optimizer, model, train_dataset)
         test_loss, test_accuracy, total_test_time = test(loss_fn, model, test_dataset)
-        print(f"\nEpoch: {epoch}\n"
+        print(f"\nEpoch: {epoch+1}\n"
               f"train loss: {train_loss:.4f} | train accuracy: {train_accuracy:.3f}% | train time: {total_train_time:.2f}."
               f"\ntest loss: {test_loss:.4f} | test accuracy: {test_accuracy:.3f}% | test time: {total_test_time:.2f}.")
+        train_dataset = create_data(NUM_TRAIN_TENSORS)
+        test_dataset = create_data(NUM_TEST_TENSORS)
     save_model(model)
 
 
