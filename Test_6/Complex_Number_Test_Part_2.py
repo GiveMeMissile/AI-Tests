6ff1a6e54dfeed42cd2a93cf41fca4067883d8cd
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2025-03-27 21:54:48 -0500

    Update Complex_Number_Test_Part_2.py
    Same as the changes to the 1st part. Yay

--- Test_6/Complex_Number_Test_Part_2.py
+++ Test_6/Complex_Number_Test_Part_2.py
@@ -7,6 +7,9 @@ import time
 from math import e, pi
 
 device = 'cuda' if torch.cuda.is_available else 'cpu'
+BATCH_SIZE = 128
+EPOCHS = 100
+PATH = "Models/complex_model_002.pth"
 
 def create_complex_tensors(number_of_values, batch):
     func = lambda z : (5*e**1j*pi/6)*z + (10*e**1j*pi/2)
@@ -28,7 +31,7 @@ def create_complex_tensors(number_of_values, batch):
 
 
 class BasicNN(nn.Module):
-    # Simple neural network. Simple
+    # Simple neural network. SIMPLE!!!
 
     def __init__(self):
         super(BasicNN, self).__init__()
@@ -47,8 +50,10 @@ def calculate_accuracy(y_pred, y):
     real_correct = 0
     imag_correct = 0
     for i in range(y.size(0)):
-        real_correct += 100 - (abs((y_pred[i].real.item()-y[i].real.item())/y[i].real.item())*100)  # PEAK MATH EQUATION!!!
-        imag_correct += 100 - (abs((y_pred[i].imag.item()-y[i].imag.item())/y[i].imag.item())*100)  # Seriously this is a peak math equaton
+        r = 100 - abs(abs(y_pred[i].real.item()-y[i].real.item())/((y[i].real.item()+y_pred[i].real.item())/2)*100)  # PEAK MATH EQUATION!!!
+        im = 100 - abs(abs(y_pred[i].imag.item()-y[i].imag.item())/((y[i].imag.item()+y_pred[i].imag.item())/2)*100)  # Seriously this is a peak math equaton
+        real_correct += r
+        imag_correct += im
     real_correct = real_correct/y.size(0)
     imag_correct = imag_correct/y.size(0)
     return (real_correct + imag_correct)/2
@@ -69,7 +74,7 @@ def train(loss_fn, model, optimizer, data):
         loss.backward()
         optimizer.step()
         train_loss += loss.item()
-        train_accuracy += calculate_accuracy(y_pred, y)
+        train_accuracy += calculate_accuracy(y_pred, y, x)
     end = time.time()
     train_loss = train_loss/len(data["X"])
     train_accuracy = train_accuracy/len(data["X"])
@@ -88,18 +93,22 @@ def test(loss_fn, model, data):
             y_pred  = model(x)
             loss = loss_fn(y_pred, y)
             test_loss += loss.item()
-            test_accuracy += calculate_accuracy(y_pred, y)
+            test_accuracy += calculate_accuracy(y_pred, y, x)
         end = time.time()
         test_loss = test_loss/len(data["X"])
         test_accuracy = test_accuracy/len(data["X"])
         return test_loss, test_accuracy, end-start
 
-
+def save_model(model, path):
+    # Save the model to a file.
+    print("Saving model...")
+    torch.save(model.state_dict(), path)
+    print("Model saved.")
 
 def main():
-    train_data = create_complex_tensors(10*(32**2), 128)
-    test_data = create_complex_tensors((32**2), 128)
-    epochs = 100
+    train_data = create_complex_tensors(10*(32**2), BATCH_SIZE)
+    test_data = create_complex_tensors((32**2), BATCH_SIZE)
+    epochs = EPOCHS
 
     model = BasicNN().to(device)
     loss_fn = nn.L1Loss()
@@ -112,8 +121,10 @@ def main():
               f"\nTest Loss: {test_loss:.4f} | Test Accuracy: {test_accuracy:.3f}% | Test Time: {test_time:.3f}")
         
         # Recreate the data every epoch because I can.
-        train_data = create_complex_tensors(10*(32**2), 128)
-        test_data = create_complex_tensors((32**2), 128)
+        train_data = create_complex_tensors(10*(32**2), BATCH_SIZE)
+        test_data = create_complex_tensors((32**2), BATCH_SIZE)
+
+    save_model(model, PATH)
 
 if __name__ == "__main__":
     main()
