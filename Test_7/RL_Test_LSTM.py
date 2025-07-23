9947de8fd1e87855e0fabf8b87950c137da0ba7a
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2025-07-23 14:07:25 -0500

    Update RL_Test_LSTM.py
    Why so SLOW AHHHH. I need to get good...

--- Test_7/RL_Test_LSTM.py
+++ Test_7/RL_Test_LSTM.py
@@ -1,7 +1,7 @@
 # Todo: 
 # 1: Reorganize code using the new AIManager class (DONE)
-# 2: Add an RL based method of learning for the AI. Such as using rewards and policy and target model. (DONEish)
-# 3: Add experiance replay for the better training of the AI. (Also save the data for future training?)
+# 2: Add an RL based method of learning for the AI. Such as using rewards and policy and target model. (DONE)
+# 3: Add experiance replay for the better training of the AI. (Also save the data for future training?)  (In Progress)
 # 4: Implement the Epsilon Greedy Policy with the utilization of the random move function.
 # 5: Add the AIs being able to know about each other and the LSTM putting a single output for all AI objects rather than how it is currently.
 # 6: Change the saving method to be compatible with the changed made in 3-5. 
@@ -13,6 +13,7 @@ import os
 import pygame
 import random
 from torch import nn
+from collections import deque
 
 # Game constants
 WINDOW_X, WINDOW_Y = 1500, 750
@@ -45,11 +46,12 @@ TEXT_FILE = SAVE_FOLDER + "/" +"model_numbers.txt"
 LEARNING_RATE = 0.001
 AI_FORWARD_TIME = 1000/40 # The AI object will change its directional vector 15 times each second thanks to this variable, this will be used for testing later
 NUM_AI_OBJECTS = 1 # Do not change till compatibility is introduced.
-NUM_SAVED_FRAMES = 60
+NUM_SAVED_FRAMES = 20
 SEQUENCE_LENGTH = 8 + 2 * GLUES
 INPUT_SHAPE = (NUM_SAVED_FRAMES, SEQUENCE_LENGTH)
 DISCOUNT_FACTOR = 0.9
 SYNC_MODEL = 10
+BATCH_SIZE = 16
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -318,9 +320,11 @@ class AI(Object):
         self.h0 = None
         self.c0 = None
         self.change_direction_timer = 0
-        self.memory = torch.zeros((NUM_SAVED_FRAMES, SEQUENCE_LENGTH), dtype=torch.float32) # AM
+        self.memory = torch.zeros((NUM_SAVED_FRAMES, SEQUENCE_LENGTH), dtype=torch.float32) # AM (Later)
+        self.previous_memory = None
+        self.action = None
 
-    def add_frame_to_memory(self, frame): # AM
+    def add_frame_to_memory(self, frame):
         self.memory = self.memory.to("cpu")
 
         for i in range(NUM_SAVED_FRAMES):
@@ -332,6 +336,7 @@ class AI(Object):
         self.memory = self.memory.to(device)
 
     def ai_move(self, ai_output):
+        self.action = torch.argmax(ai_output) + 1
         directional_vector = self.get_directional_vector(ai_output)
         if (directional_vector[0]):
             self.dy -= ACCELERATION
@@ -406,6 +411,23 @@ class AI(Object):
         return False
 
 
+class TrainingData:
+    # This class will store some of the training data for future training WOHOOOOOOO!!!
+    def __init__(self, max_length):
+        self.data = deque([], maxlen=max_length)
+
+    def append(self, transition):
+        # A transition is a tuple which contains information used for training the AI.
+        # Transition = (AI memory, action made by AI, New memory after action was made, Reward earned from the action, saved h0, saved c0)
+        self.data.append(transition)
+
+    def get_sample(self, batch_size):
+        return random.sample(self.data, batch_size)
+    
+    def __len__(self):
+        return len(self.data)
+
+
 class LSTM(nn.Module):
     # We will be using an LSTM model in this experiment. This LSTM will control the AI objects.
 
@@ -418,7 +440,7 @@ class LSTM(nn.Module):
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.output_layer = nn.Linear(hidden_size, output_size)
 
-    def forward(self, x, h0, c0):
+    def forward(self, x, h0=None, c0=None):
         # Input tensor ideas: sequence = [aix, aiy, dx, dy, player_x, player_y, glue_x1, glue_y1, glue_x2, glue_y2, ...]
         # where dx and dy are the player's velocity, player_x and player_y are the player's position,
         # Input_Tensor = [NUM_SAVED_FRAMES, sequence length]. Where there will be NUM_SAVED_FRAMES amount of sequances in the tensor.
@@ -437,7 +459,7 @@ class LSTM(nn.Module):
 
         # Output tensor: [action 1, action 2, action 3, action 4, action 5, action 6, action 7, action 8, action 9]
         # Note: the actions are structured as a binary tensor which determine which direction the thing go. [up, down, left, right]
-        # 0 = False, 1 = True, -1 = My Life
+        # 0 = False, 1 = True
         # action 1 = [0, 0, 0, 0] Going nowhere (Like my life)
         # action 2 = [1, 0, 0, 0] Going up
         # action 3 = [0, 1, 0, 0] Going down
@@ -447,7 +469,7 @@ class LSTM(nn.Module):
         # action 7 = [1, 0, 0, 1] Going Up + Right
         # action 8 = [0, 1, 1, 0] Going Down + Left
         # action 9 = [0, 1, 0, 1] Going Down + Right
-        # where up, down, left, and right are the AI's actions to move. Each variable will be between 0 and 1. 
+        # where up, down, left, and right are the AI's actions to move. Each variable will be a binary value of 0 or 1/False or True. 
         return x, h0, c0
     
 
@@ -464,6 +486,7 @@ class AIManager:
         self.total_reward = 0
         self.loss_fn = nn.MSELoss()
         self.frame_count = 0
+        self.data_manager = TrainingData(max_length=1000)
 
     def create_ais(self, num_ais):
         ais = []
@@ -478,7 +501,7 @@ class AIManager:
                 ))
         return ais
     
-    def create_tensor(self, ai):
+    def update_memory(self, ai):
         x, y = self.player.get_center()
         x_ai, y_ai = ai.get_center()
         X = torch.tensor([
@@ -510,19 +533,13 @@ class AIManager:
             self.target_model.load_state_dict(self.policy_model.state_dict()) 
 
         for ai in self.ai_list:
-            self.create_tensor(ai)
-            q_values, ai.h0, ai.c0 = self.policy_model(ai.memory.to(device), ai.h0, ai.c0)
+            q_values, _, _ = self.policy_model(ai.memory.to(device))
             q_values = q_values.squeeze(0)
-            q_targets, _, _ = self.target_model(ai.memory.to(device), ai.h0, ai.c0)
-            q_targets = q_targets.squeeze(0)
+            ai.previous_memory = ai.memory
             ai.ai_move(q_values)
-            self.train_ai(ai, q_values, q_targets)
 
-    def train_ai(self, ai, q_values, q_targets): # AM
-        # Trains the AI using Q-learning stuff I learned from the Internet...
-        # Im so bad at ts lol. For now...
-
-        # Calculate the reward
+    def save_data(self, ai):
+        # Create Rewards
         reward = 0
         if ai.in_glue:
             reward -= 50
@@ -533,18 +550,31 @@ class AIManager:
         if ai.moving_into_wall(False):
             reward -= 10
 
-        # Modify the target value to match the actual reward.
-        # print(f"Q-values: {q_values}\nPrevious Target: {q_targets}\nAction: {q_values.argmax()}\nReward: {reward}\n")
-        target = torch.FloatTensor(reward + DISCOUNT_FACTOR * self.target_model(ai.memory.to(device), ai.h0, ai.c0)[0].max().to("cpu"))
-        q_targets[q_values.argmax()] = target
-        self.total_reward += reward
+        self.data_manager.append((ai.previous_memory, ai.action, ai.memory, reward))
 
-        # Normal PyTorch Stuff
-        self.optimizer.zero_grad()
-        loss = self.loss_fn(q_values, q_targets)
-        # print(f"New Target: {q_targets}\nLoss: {loss}\n")
-        loss.backward()
-        self.optimizer.step()
+    def train_ai(self, ai):
+
+        batch = self.data_manager.get_sample(BATCH_SIZE)
+        for state, action, new_state, reward in batch:
+            # Modify the target value to match the actual reward.
+            # print(f"Q-values: {q_values}\nPrevious Target: {q_targets}\nAction: {q_values.argmax()}\nReward: {reward}\n")
+
+            q_values, _, _ = self.policy_model(state.to(device))
+            q_values = q_values.squeeze(0)
+
+            q_targets, _, _ = self.target_model(state.to(device))
+            q_targets = q_targets.squeeze(0)
+
+            with torch.no_grad():
+                target = torch.FloatTensor(reward + DISCOUNT_FACTOR * self.target_model(new_state.to(device), ai.h0, ai.c0)[0].max().to("cpu"))
+            q_targets[action] = target
+
+            # Normal PyTorch Stuff
+            self.optimizer.zero_grad()
+            loss = self.loss_fn(q_values, q_targets)
+            # print(f"New Target: {q_targets}\nLoss: {loss}\n")
+            loss.backward()
+            self.optimizer.step()
 
     def load_model(self):
         # This function checks if one of the saved models can be loaded, and if it can the function will load the model.
@@ -612,6 +642,7 @@ def draw_game(player, glues, time, ai_manager):
 
 def main():
     sys.setrecursionlimit(100000)
+    num_frames = 0
     running = True
     clock = pygame.time.Clock()
     player = Player(PLAYER_DIM, PLAYER_DIM, window, WHITE, objects=[], distance=None, x=WINDOW_X/2-PLAYER_DIM/2, y=WINDOW_Y/2-PLAYER_DIM/2)
@@ -639,6 +670,12 @@ def main():
             game_end(ai_manager)
             running = False
 
+        # AI moves first
+        ai_manager.frame_count += 1
+        ai_manager.move_ais()
+        for ai in ai_manager.ai_list:
+            ai.check_for_collisions(current_time, player)
+
         for glue in glues:
             glue.check_for_collisions(ai_manager.ai_list + [player], current_time)
             glue.check_bounds()
@@ -648,16 +685,21 @@ def main():
                 glue.movement_timer = current_time
             else:
                 glue.move()
-        ai_manager.frame_count += 1
-        ai_manager.move_ais()
-        for ai in ai_manager.ai_list:
-            ai.check_for_collisions(current_time, player)
 
         player.player_move(current_time)
         if player.health <= 0:
             game_end(ai_manager)
             running = False
+
+        # Saved the changes made to the enviorment, Save the needed information for training, Then Train the AI.
+        for ai in ai_manager.ai_list:
+            ai_manager.update_memory(ai)
+            ai_manager.save_data(ai)
+            if num_frames >= BATCH_SIZE:
+                ai_manager.train_ai(ai)
+
         draw_game(player, glues, current_time - previous_time, ai_manager)
+        num_frames += 1
         clock.tick(60)
 
 
